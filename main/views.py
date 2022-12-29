7b24088fc948ff6d4fd39c8c24c6c584e28fae9f
AtstecP <109477937+AtstecP@users.noreply.github.com>
2022-12-29 08:37:01 +0500

    first commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'main/home.html')
+
+
+def info(request):
+    return render(request, 'main/info.html')
